9d3190d834c6636927cab124cecaa9a9e8690cac
jaeyoungjang2 <lion85488@gmail.com>
2021-07-26 22:36:43 +0900

    feat: 관람객 추가

--- src/main/java/ticket/Audience.java
+++ src/main/java/ticket/Audience.java
@@ -1,2 +1,21 @@
-package ticket;public class Audience {
+package ticket;
+
+public class Audience {
+    // 관람객은 가방을 들고 있다.
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
 }
