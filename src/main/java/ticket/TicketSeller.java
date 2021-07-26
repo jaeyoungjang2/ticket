513ea2bf3a7b9943d99811d2b2358f7b8fde39ae
jaeyoungjang2 <lion85488@gmail.com>
2021-07-26 22:36:27 +0900

    feat: 티켓 판매자 추가

--- src/main/java/ticket/TicketSeller.java
+++ src/main/java/ticket/TicketSeller.java
@@ -1,2 +1,16 @@
-package ticket;public class TicketSeller {
-}
+package ticket;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public void sellTo(Audience audience) {
+        Ticket ticket = ticketOffice.getTicket();
+        Long fee = audience.buy(ticket);
+        ticketOffice.plusAmount(fee);
+        }
+    }
+
