f0afd45903068ffeff62f39b7d3674bb19cbc714
jaeyoungjang2 <lion85488@gmail.com>
2021-07-29 22:23:54 +0900

    add bag

--- src/main/java/ticket/Bag.java
+++ src/main/java/ticket/Bag.java
@@ -1,2 +1,40 @@
-package ticket;public class Bag {
+package ticket;
+
+public class Bag {
+    private Long amount;
+    private Ticket ticket;
+    private Invitation invitation;
+
+    public Bag(Long amount) {
+        this.amount = amount;
+    }
+
+    public Bag(Long amount, Invitation invitation) {
+        this.amount = amount;
+        this.invitation = invitation;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public boolean hasTicket() {
+        return ticket != null;
+    }
+
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public Invitation getInvitation() {
+        return invitation;
+    }
+
+    public void minusAmount(Long fee) {
+        amount -= fee;
+    }
+
+    public void plusAmount(Long fee) {
+        amount += fee;
+    }
 }
