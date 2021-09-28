445c2ec4925ad749902d047b7c5bf3f848194500
jaeyoungjang2 <lion85488@gmail.com>
2021-09-28 22:49:50 +0900

    feat: 관람객 클래스 생성 티켓을 구매하는 메서드 생성 티켓을 구매하는 경우 관람객이 소지하고 있는 돈 감소

--- src/main/java/day20210928/Audience.java
+++ src/main/java/day20210928/Audience.java
@@ -1,2 +1,22 @@
-package day20210928;public class Audience {
+package day20210928;
+
+public class Audience {
+
+    private final Bag bag;
+    private final Money money;
+    private final Invitation invitation;
+
+    public Audience() {
+        this.invitation = new Invitation();
+        this.money = new Money();
+        this.bag = new Bag();
+    }
+
+    public void buyTicket(int ticketPrice) {
+        money.spend(ticketPrice);
+    }
+
+    public Money money() {
+        return money;
+    }
 }
