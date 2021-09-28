9a240ced49a1a6cdae29723a1c2c19e024c15c01
jaeyoungjang2 <lion85488@gmail.com>
2021-09-28 22:50:28 +0900

    feat: Money 클래스 생성 현재는 관람객의 돈으로 사용하고 있음

--- src/main/java/day20210928/Money.java
+++ src/main/java/day20210928/Money.java
@@ -1,2 +1,34 @@
-package day20210928;public class Money {
+package day20210928;
+
+import java.util.Objects;
+
+public class Money {
+    private int amount;
+
+    public Money() {}
+
+    protected Money(int amount) {
+        this.amount = amount;
+    }
+
+    public void spend(int ticketPrice) {
+        amount -= ticketPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
 }
