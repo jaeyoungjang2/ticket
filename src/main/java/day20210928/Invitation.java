4623328d0ca606385526d16f3462559660a89df2
jaeyoungjang2 <lion85488@gmail.com>
2021-09-28 22:51:03 +0900

    feat: 초대장 클래스 생성 기본 날짜는 -1로 지정되어 있음 -1이란 초기 설정값이고, 초대장이 없는 경우를 의미

--- src/main/java/day20210928/Invitation.java
+++ src/main/java/day20210928/Invitation.java
@@ -1,9 +1,10 @@
 package day20210928;
 
 public class Invitation {
+    private static final int initDate = -1;
     private final int date;
 
-    public Invitation(int date) {
-        this.date = date;
+    public Invitation() {
+        this.date = initDate;
     }
 }
